9794ddb69e66f809ef0363a225afa1f9fa309a5d
JasonOuyang80 <jasonouyang80@gmail.com>
2019-12-16 15:27:29 -0800

    Updates

---
+++ precache-manifest.5edc9a77907bdf7188861e73bdf47a3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24c244d22a28af13f8c64a1a24f0791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98dc6526ed6906e112af",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ffcffe346a022c15cc8d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98dc6526ed6906e112af",
+    "url": "/static/js/2.eda1cfe2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eda1cfe2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffcffe346a022c15cc8d",
+    "url": "/static/js/main.c1916065.chunk.js"
+  },
+  {
+    "revision": "fd6bdb3918b1bc7f1214",
+    "url": "/static/js/runtime-main.7f4b5bf6.js"
+  }
+]);
